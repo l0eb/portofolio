4a51b87ab3c51d7a67de0f786bb74996a33b37a5
l0eb <mrloebdesanta@gmail.com>
2023-07-30 20:51:36 +0530

    flex earth properties

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -19,7 +19,7 @@ const Contact = () => {
   const handleChange = () => {};
 
   return (
-    <div className="xl:mt-12 xl:flex-row flex-column-reverse flex gap-10 overflow-hidden">
+    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
         className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
